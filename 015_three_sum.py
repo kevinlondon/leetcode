30a2a84d225ec1a96e4d3710ab653d83eaa4e262
Kevin London <kevinlondon@gmail.com>
2021-12-04 13:39:08 -0800

    Three sum, semi-brute force O(n^2)

--- 015_three_sum.py
+++ 015_three_sum.py
@@ -1,33 +1,62 @@
+from typing import List, Dict, Set, Tuple
+from collections import defaultdict
+
+
 class Solution:
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        nums.sort()
-        ans = []
-        for i in range(len(nums) - 2):
-            if i == 0 or (i > 0 and nums[i] != nums[i-1]):
-                lo = i+1
-                hi = len(nums) - 1
-                total = 0 - nums[i]
-                
-                while (lo < hi):
-                    if nums[lo] + nums[hi] == total:
-                        ans.append([nums[i], nums[lo], nums[hi]])
-                        while lo < hi and nums[lo] == nums[lo+1]:
-                            lo += 1
-                        
-                        while lo < hi and nums[hi] == nums[hi-1]:
-                            hi -= 1
-                        
-                        lo += 1
-                        hi -= 1
-                    elif nums[lo] + nums[hi] < total:
-                        lo += 1
-                    else:
-                        hi -= 1
-            
-        return ans
-            
-        
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        matches = set()
+
+        nums = self.trimNums(nums)
+        sums = self.twoSum(nums)
+
+        for idx, value in enumerate(nums):
+            inverse = -value
+            if inverse not in sums:
+                continue
+
+            pairs = sums[inverse]
+            for pair in pairs:
+                if idx in pair:
+                    continue
+
+                match = tuple(sorted([nums[idx], nums[pair[0]], nums[pair[1]]]))
+                matches.add(match)
+
+
+        return [list(match) for match in matches]
+
+    def trimNums(self, nums: List[int]) -> List[int]:
+        trimmed_nums = []
+        seen = defaultdict(int)
+        for num in nums:
+            if seen.get(num, 0) > 3:
+                continue
+            trimmed_nums.append(num)
+            seen[num] += 1
+
+        return trimmed_nums
+
+    def twoSum(self, nums: List[int]) -> Dict[int, Tuple[List[int], Set[int]]]:
+        sums = defaultdict(list)
+
+        for index, value in enumerate(nums[:-1]):
+            for index2, value2 in enumerate(nums[index+1:], start=index+1):
+                sums[value + value2].append([index, index2])
+
+        return sums
+
+
+sol = Solution()
+
+def test_base_case():
+    nums = [-1,0,1,2,-1,-4]
+    assert sol.threeSum(nums) == [[-1, 0, 1], [-1, -1, 2]]
+
+def test_empty_case():
+    assert sol.threeSum([]) == []
+
+def test_single_item():
+    assert sol.threeSum([0]) == []
+
+def test_all_same_item():
+    assert sol.threeSum([0] * 1000) == [[0, 0, 0]]
