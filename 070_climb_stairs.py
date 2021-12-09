d2fbbb547bd629c51842ded85d6cb3543a01d453
Kevin London <kevinlondon@gmail.com>
2021-12-08 21:01:09 -0800

    Updating instead of making a new file.

--- 070_climb_stairs.py
+++ 070_climb_stairs.py
@@ -1,18 +1,21 @@
-class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        a, b = 1, 2
-        if n == 1:
-            return a
-        elif n == 2:
-            return b
-        
-        for i in range(2, n):
-            a, b = b, a+b
-        
-        return b
-            
-        
+import pytest
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2: return n
+        x, y = 1, 2
+
+        for _ in range(3, n+1):
+            x, y = y, x + y
+
+        return y
+
+
+@pytest.mark.parametrize("n, expected", [
+    (2, 2),
+    (3, 3),
+    (4, 5)
+])
+def test_sol(n, expected):
+    assert Solution().climbStairs(n) == expected
