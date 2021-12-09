89760881b138042d5398dfecf2932ebc16fff9bd
Kevin London <kevinlondon@gmail.com>
2021-12-08 19:52:30 -0800

    Counting bits non-cheating.

--- 338_counting_bits.py
+++ 338_counting_bits.py
@@ -1,11 +1,7 @@
 class Solution:
-    def countBits(self, num):
-        """
-        :type num: int
-        :rtype: List[int]
-        """
-        bits = []
-        for x in range(num+1):
-            bits.append(bin(x).count('1'))
-        
+    def countBits(self, n: int) -> List[int]:
+        bits = [0] * (n+1)
+        for i in range(1, n+1):
+            bits[i] = bits[i >> 1] + (i & 1)
+
         return bits
