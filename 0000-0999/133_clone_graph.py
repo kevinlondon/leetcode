3841acc1b2187cb56cb5a3f4d80607685908a845
Kevin London <kevinlondon@gmail.com>
2022-01-06 21:09:38 -0800

    Updating clone graph to be a more concise form of the question.

--- 0000-0999/133_clone_graph.py
+++ 0000-0999/133_clone_graph.py
@@ -6,29 +6,19 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-from queue import Queue
-
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        adjacency_list, nodes = {}, {}
+        nodes = {}
+        return self.clone(node, nodes)
 
+    def clone(self, node: 'Node', nodes: dict) -> 'Node':
         if not node:
             return
 
-        self.buildNodes(node, nodes, adjacency_list)
-        self.buildNeighbors(nodes, adjacency_list)
+        if node.val in nodes:
+            return nodes[node.val]
+
+        nodes[node.val] = Node(val=node.val)
+        nodes[node.val].neighbors = [self.clone(neighbor, nodes) for neighbor in node.neighbors]
 
         return nodes[node.val]
-
-    def buildNodes(self, node: 'Node', nodes, adjacency_list):
-        adjacency_list[node.val] = [node.val for node in node.neighbors]
-        nodes[node.val] = Node(val=node.val)
-
-        for neighbor in node.neighbors:
-            if neighbor.val not in adjacency_list:
-                self.buildNodes(neighbor, nodes, adjacency_list)
-
-    def buildNeighbors(self, nodes, adjacency_list):
-        for value, neighbors in adjacency_list.items():
-            nodes[value].neighbors = [nodes[neighbor] for neighbor in neighbors]
-
