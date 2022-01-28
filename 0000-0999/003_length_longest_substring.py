03a94ffac0091a70f010b728d1c20f62d6f5a6be
Kevin London <kevinlondon@gmail.com>
2022-01-27 20:05:01 -0800

    Longest substring

--- 0000-0999/003_length_longest_substring.py
+++ 0000-0999/003_length_longest_substring.py
@@ -1,19 +1,22 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        in_string = set()
         longest = 0
-        mapping = {}
-        
-        index2 = 0
-        for index in xrange(len(s)):
-            character = s[index]
-            if character in mapping:
-                index2 = max(index2, mapping[character] + 1)
-                
-            mapping[character] = index
-            longest = max(longest, index - index2 + 1)
-            
+
+        head = 0
+
+        for i, letter in enumerate(s):
+            if letter in in_string:
+                longest = max(longest, len(in_string))
+                remove = s[head]
+                while remove != letter and head < i:
+                    in_string.remove(remove)
+                    head += 1
+                    remove = s[head]
+                head += 1
+                in_string.remove(remove)
+
+            in_string.add(letter)
+
+        longest = max(longest, len(in_string))
         return longest
