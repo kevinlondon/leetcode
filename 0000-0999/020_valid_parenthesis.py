e9ac9b589df41f0fe9b58387f05ad72ce83f980a
Kevin London <kevinlondon@gmail.com>
2022-01-28 22:06:05 -0800

    TIL: alnum

--- 0000-0999/020_valid_parenthesis.py
+++ 0000-0999/020_valid_parenthesis.py
@@ -10,10 +10,8 @@ class Solution:
         for c in s:
             if c not in self.opposites:
                 openers.append(c)
-            elif not openers or openers[-1] != self.opposites[c]:
+            elif not openers or openers.pop() != self.opposites[c]:
                 return False
-            else:
-                openers.pop(-1)
 
         if openers:
             return False
