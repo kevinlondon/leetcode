ebd13c1250477adbdcb5111278327a3ef305b6ac
Kevin London <kevinlondon@gmail.com>
2022-01-28 21:50:53 -0800

    Simpler grouped anagrams.

--- 0000-0999/049_group_anagrams.py
+++ 0000-0999/049_group_anagrams.py
@@ -1,17 +1,8 @@
 class Solution:
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        anagrams = {}
-        
-        for string in strs:
-            sorted_str = "".join(sorted(string))
-            if sorted_str in anagrams:
-                anagrams[sorted_str].append(string)
-            else:
-                anagrams[sorted_str] = [string, ]
-            
-        anagram_lists = [anagram_list for anagram, anagram_list in anagrams.items()]
-        return anagram_lists
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ana = defaultdict(list)
+
+        for word in strs:
+            ana[str(sorted(word))].append(word)
+
+        return ana.values()
