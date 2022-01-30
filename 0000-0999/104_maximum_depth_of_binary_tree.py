8c23bb5229b3ddf3ff40613b482eb0adf0f8a316
Kevin London <kevinlondon@gmail.com>
2022-01-29 21:10:46 -0800

    BFS for max depth

--- 0000-0999/104_maximum_depth_of_binary_tree.py
+++ 0000-0999/104_maximum_depth_of_binary_tree.py
@@ -1,19 +1,35 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
-class Solution(object):
-    def maxDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+from queue import Queue
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        level = Queue()
+        next_level = Queue()
+
+        level.put(root)
+
         if not root:
             return 0
-        
-        left = self.maxDepth(root.left)
-        right = self.maxDepth(root.right)
-        return max([left, right]) + 1
+
+        max_level = 1
+
+        while not level.empty():
+            node = level.get()
+            if not node:
+                continue
+
+            if node.left: next_level.put(node.left)
+            if node.right: next_level.put(node.right)
+
+            if level.empty() and not next_level.empty():
+                level = next_level
+                max_level += 1
+                next_level = Queue()
+
+        return max_level
