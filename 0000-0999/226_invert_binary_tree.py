e22d795a8e384743ae1eca241652cad98dd905ec
Kevin London <kevinlondon@gmail.com>
2022-01-29 21:23:15 -0800

    Reverting binary tree several years later.

--- 0000-0999/226_invert_binary_tree.py
+++ 0000-0999/226_invert_binary_tree.py
@@ -1,21 +1,15 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def invertTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return
-        
-        root.left, root.right = root.right, root.left
+
         self.invertTree(root.left)
         self.invertTree(root.right)
+        root.right, root.left = root.left, root.right
         return root
-        
