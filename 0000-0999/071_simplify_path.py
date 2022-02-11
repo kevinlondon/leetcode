533f7ed52de1b64e656063216ad70550d4fdd397
Kevin London <kevinlondon@gmail.com>
2022-02-10 22:23:16 -0800

    More nuanced simplify path.

--- 0000-0999/071_simplify_path.py
+++ 0000-0999/071_simplify_path.py
@@ -2,30 +2,14 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         dirs = []
 
-        open_path = []
-        def is_cur_dir():
-            return len(open_path) == 1 and open_path[0] == '.'
-
-        def is_par_dir():
-            return len(open_path) == 2 and open_path[0] == '.' and open_path[1] == '.'
-
-        for letter in path:
-            if letter == '/':
-                if open_path:
-                    if is_par_dir():
-                        if dirs:
-                            dirs.pop()
-                    elif not is_cur_dir():
-                        dirs.append(''.join(open_path))
-                    open_path = []
-            else:
-                open_path.append(letter)
-        else:
-            if is_par_dir():
+        for p in path.split('/'):
+            if p == '.' or not p:
+                continue
+            elif p == '..':
                 if dirs:
                     dirs.pop()
-            elif not is_cur_dir() and open_path:
-                dirs.append(''.join(open_path))
+            else:
+                dirs.append(p)
 
         return '/' + '/'.join(dirs)
 
