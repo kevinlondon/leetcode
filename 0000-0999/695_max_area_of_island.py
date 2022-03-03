0a9c1c2fe92e510bd1c9cf9d112bc6e9f7b7982d
Kevin London <kevinlondon@gmail.com>
2022-03-02 21:18:41 -0800

    DFS max island area

--- 0000-0999/695_max_area_of_island.py
+++ 0000-0999/695_max_area_of_island.py
@@ -6,31 +6,18 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         max_seen = 0
 
+        def dfs(x, y):
+            if 0 <= x < len(grid[0]) and 0 <= y < len(grid) and grid[y][x]:
+                grid[y][x] = 0
+                areas = 1
+                for delta_x, delta_y in self.directions:
+                    areas += dfs(x+delta_x, y+delta_y)
+                return areas
+            return 0
+
         for y in range(len(grid)):
             for x in range(len(grid[0])):
                 if grid[y][x] == 1:
-                    max_seen = max(max_seen, self.getIslandArea(grid, x, y))
+                    max_seen = max(max_seen, dfs(x, y))
 
         return max_seen
-
-    def getIslandArea(self, grid: List[List[int]], x: int, y: int) -> int:
-        queue = Queue()
-        queue.put((x, y))
-        area = 0
-
-        while not queue.empty():
-            n_x, n_y = queue.get()
-
-            if not (0 <= n_x < len(grid[0]) and 0 <= n_y < len(grid)):
-                continue
-            elif grid[n_y][n_x] != 1:
-                continue
-
-            grid[n_y][n_x] = -1
-            area += 1
-
-            for delta_x, delta_y in self.directions:
-                i, j = n_x + delta_x, n_y + delta_y
-                queue.put((i, j))
-
-        return area
