f50d30f69d157c753d56917f9476743052aa4aeb
Kevin London <kevinlondon@gmail.com>
2022-03-23 20:40:09 -0700

    A more optimal approach for max profit scheduling.

--- 1000-2000/1235_max_profit_in_scheduling.py
+++ 1000-2000/1235_max_profit_in_scheduling.py
@@ -1,40 +1,19 @@
+Job = namedtuple('Job', ['start', 'end', 'profit'])
+
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        """
-        One idea is to decompose this to the maximum possible values at a given time,
-        which is only possible when the time completes.
-        We could sort by the max endTime and evaluate that.
+        jobs = sorted([Job(startTime[i], endTime[i], profit[i]) for i in range(len(startTime))])
+        startTimes = [job.start for job in jobs]
 
-        Maybe we need to track the overall max seen too?
-        At each time, the maximum value will be either the max
-        that we've seen so far or the value of the current time plus
-        the max at the time we saw previously, right?
-        """
+        @lru_cache(None)
+        def dp(i):
+            if i == len(jobs):
+                return 0
 
-        if not startTime:
-            return 0
+            job = jobs[i]
+            # So either we take the job and look for the next or we skip this one
+            nextJob = bisect.bisect_left(startTimes, job.end)
+            maxProfit = max(dp(i+1), job.profit + dp(nextJob))
+            return maxProfit
 
-        Job = namedtuple('Job', ['start', 'end', 'profit'])
-
-        jobs = [Job(startTime[i], endTime[i], profit[i]) for i in range(len(startTime))]
-        jobs = sorted(jobs, key=lambda x: x.end)
-
-        max_time_profit = defaultdict(int)
-        j_index = 0
-        max_prof_seen = 0
-
-        for i, job in enumerate(jobs):
-            last_job = job
-            j = i
-            while j >= 0 and last_job.end > job.start:
-                j -= 1
-                last_job = jobs[j]
-
-            if last_job.end <= job.start:
-                max_prof_seen = max(max_prof_seen, max_time_profit[last_job.end]+job.profit)
-            else:
-                max_prof_seen = max(max_prof_seen, job.profit)
-
-            max_time_profit[job.end] = max_prof_seen
-
-        return max_prof_seen
+        return dp(0)
